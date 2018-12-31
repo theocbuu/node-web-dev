2dfc8e8e8ac76fc511ad13214d5aadf7edf90325
Giang Phan <giangphan@GP-Mac13.local>
2018-12-31 09:56:06 -0500

    moved files to chap02

--- chap02/ls2.js
+++ chap02/ls2.js
@@ -3,7 +3,9 @@ const util = require('util');
 const fs_readdir = util.promisify(fs.readdir);
 
 (async() => {
-    const files = await fs_readdir('.');
+    var dir = '.';
+    if(process.argv[2]) dir=process.argv[2];
+    const files = await fs_readdir(dir);
     for(let fn of files){
         console.log(fn);
     }
